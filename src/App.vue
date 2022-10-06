c9428fa477b2b610a59554709d794b23c0c895d2
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-06 20:24:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
